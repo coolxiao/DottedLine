74cf654c387f4e8667484a4c4f5590aa40c7d4bd
jiexiaoqiang <877023699@qq.com>
2017-04-12 16:49:50 +0800

    s

--- app/src/main/java/com/jiexiaoqiang/dottedline/MainActivity.java
+++ app/src/main/java/com/jiexiaoqiang/dottedline/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jiexiaoqiang.dottedline;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NumberProgressView mView = (NumberProgressView) findViewById(R.id.numberProgressView);
+        DottedLineView mView = (DottedLineView) findViewById(R.id.numberProgressView);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
